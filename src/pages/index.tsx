083e98705b0b1f09260c1675240cc4033988d2da
Eduardo <princesherlockk@gmail.com>
2023-12-14 15:55:11 -0300

    [Utilities]-feat- implement design home and color variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import LayoutPage from "@/components/LayoutPage"
+import Image from "next/image"
+
+import styles from './Home.module.scss'
+
+import Logo from '../../public/assets/logo.png'
 
 export default function Home() {
   return(
     <>
-      <LayoutPage>Hello</LayoutPage>
+      <LayoutPage>
+        <div className={styles.container}>
+          <div className={styles.subContainer}>
+            <Image src={Logo} alt="logo"className={styles.logo}/>
+            <h1>Conjunto de aplicações de utilidades diárias</h1>
+          </div>
+        </div>
+      </LayoutPage>
     </>
   )
 }
