58c94939c73274f436a82d79e942fb26974517bb
Eduardo <princesherlockk@gmail.com>
2024-01-04 16:36:19 -0300

    [Utilities]-feat- highlighted in results IMC

--- src/pages/imc/index.tsx
+++ src/pages/imc/index.tsx
@@ -19,6 +19,29 @@ export default function IMC() {
     setHeigth('');
   }
 
+  var classStyleLine1 = styles.item
+  var classStyleLine2 = styles.item
+  var classStyleLine3 = styles.item
+  var classStyleLine4 = styles.item
+  var classStyleLine5 = styles.item
+
+  if(result < 18.5 && result != 0){
+    var classStyleLine1 = styles.itemPress
+  }
+  if(result >= 18.5 && result <= 24.9){
+    var classStyleLine2 = styles.itemPress
+  }
+  if(result >= 25 && result <= 29.9){
+    var classStyleLine3 = styles.itemPress
+  }
+  if(result >= 30 && result <= 39.9){
+    var classStyleLine4 = styles.itemPress
+  }
+  if(result > 40){
+    var classStyleLine5 = styles.itemPress
+  }
+
+
   return (
     <LayoutPage>
       <div className={styles.container}>
@@ -52,27 +75,27 @@ export default function IMC() {
           <div className={styles.container}>
             <div className={styles.subContainer}>
               <div className={styles.index}>IMC</div>
-              <div className={styles.item}>MENOR QUE 18,5</div>
-              <div className={styles.item}>ENTRE 18,5 E 24,9</div>
-              <div className={styles.item}>ENTRE 25,0 E 29,9</div>
-              <div className={styles.item}>ENTRE 30,0 E 39,9</div>
-              <div className={styles.item}>MAIOR QUE 40,0</div>
+              <div className={classStyleLine1}>MENOR QUE 18,5</div>
+              <div className={classStyleLine2}>ENTRE 18,5 E 24,9</div>
+              <div className={classStyleLine3}>ENTRE 25,0 E 29,9</div>
+              <div className={classStyleLine4}>ENTRE 30,0 E 39,9</div>
+              <div className={classStyleLine5}>MAIOR QUE 40,0</div>
             </div>
             <div className={styles.subContainer}>
               <div className={styles.index}>CLASSIFICAÇÃO</div>
-              <div className={styles.item}>MAGREZA</div>
-              <div className={styles.item}>NORMAL</div>
-              <div className={styles.item}>SOBREPESO</div>
-              <div className={styles.item}>OBESIDADE</div>
-              <div className={styles.item}>OBESIDADE GRAVE</div>
+              <div className={classStyleLine1}>MAGREZA</div>
+              <div className={classStyleLine2}>NORMAL</div>
+              <div className={classStyleLine3}>SOBREPESO</div>
+              <div className={classStyleLine4}>OBESIDADE</div>
+              <div className={classStyleLine5}>OBESIDADE GRAVE</div>
             </div>
             <div className={styles.subContainer}>
               <div className={styles.index}>OBESIDADE</div>
-              <div className={styles.item}>0</div>
-              <div className={styles.item}>0</div>
-              <div className={styles.item}>I</div>
-              <div className={styles.item}>II</div>
-              <div className={styles.item}>III</div>
+              <div className={classStyleLine1}>0</div>
+              <div className={classStyleLine2}>0</div>
+              <div className={classStyleLine3}>I</div>
+              <div className={classStyleLine4}>II</div>
+              <div className={classStyleLine5}>III</div>
             </div>
           </div>
           <div className={styles.result}>Resultado do seu IMC: {result.toFixed(2)}</div>
